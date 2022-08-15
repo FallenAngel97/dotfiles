39b75d882e4a97c5fc861fc3bf403da678a23da9
Lex <mr.x97@mail.ru>
2022-08-15 13:54:56 +0300

    Added for auto installing treesitter languages

--- init.lua
+++ init.lua
@@ -17,6 +17,18 @@ require('packer').startup(function()
 			run = ':TSUpdate',
 			config = function ()
 				require'nvim-treesitter.configs'.setup {
+						ensure_installed = {
+							"javascript",
+							"lua",
+							"typescript",
+							"python",
+							"dart",
+							"css",
+							"scss",
+							"dockerfile",
+							"rego"
+						},
+						auto_install = true,
 						highlight = {
 								enable = true,
 						},
