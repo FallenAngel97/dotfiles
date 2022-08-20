1bf515ad96826ca1b5dc311ad62ef23765821aa9
Lex <mr.x97@mail.ru>
2022-08-20 16:59:28 +0300

    Added config for showing color sign

--- init.lua
+++ init.lua
@@ -5,8 +5,23 @@ require 'android'
 require('packer').startup(function()
     use 'wbthomason/packer.nvim'
     use 'kyazdani42/nvim-web-devicons';
+		use { 
+			"rcarriga/nvim-dap-ui",
+			requires = {"mfussenegger/nvim-dap"},
+			config = function()
+				require("dapui").setup()
+			end
+		}
     use 'tpope/vim-fugitive'
-    use 'lewis6991/gitsigns.nvim'
+    use {
+			'lewis6991/gitsigns.nvim',
+			config = function ()
+				require('gitsigns').setup {
+					current_line_blame = true,
+					numhl = true,
+				}
+			end
+		}
     use {
 			'editorconfig/editorconfig-vim',
 			opt = true,
@@ -41,11 +56,33 @@ require('packer').startup(function()
 			end
 		}
     use { 'prettier/vim-prettier', run = 'yarn install --frozen-lockfile --production' }
-    use 'sainnhe/sonokai'
+    use {
+			'sainnhe/sonokai',
+			config = function()
+				vim.api.nvim_command("colorscheme sonokai")
+			end
+		}
     use 'neovim/nvim-lspconfig'
     use 'nvim-lua/popup.nvim'
     use 'nvim-lua/plenary.nvim'
-    use 'nvim-telescope/telescope.nvim'
+    use {
+			'nvim-telescope/telescope.nvim',
+			config = function ()
+				require('telescope').setup{
+					pickers = {
+						find_files = {
+							previewer = false,
+						}
+					},
+					 defaults = {
+							 layout_config = {
+								 prompt_position = 'top',
+							 },
+							 sorting_strategy = 'ascending',
+					 },
+				}
+			end
+		}
 		use 'onsails/lspkind.nvim'
     use 'hrsh7th/cmp-nvim-lsp'
 		use 'hrsh7th/cmp-buffer'
@@ -53,13 +90,62 @@ require('packer').startup(function()
 		use 'hrsh7th/cmp-cmdline'
 		use 'hrsh7th/cmp-vsnip'
 		use 'hrsh7th/vim-vsnip'
-		use 'hrsh7th/nvim-cmp'
+		use {
+			'hrsh7th/nvim-cmp',
+			config = function()
+				local cmp = require'cmp'
+				cmp.setup({
+					snippet = {
+						expand = function(args)
+							vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+						end,
+					},
+					window = {
+						completion = {
+							winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+							col_offset = -3,
+							side_padding = 0,
+						},
+					},
+					mapping = cmp.mapping.preset.insert({
+						['<C-b>'] = cmp.mapping.scroll_docs(-4),
+						['<C-f>'] = cmp.mapping.scroll_docs(4),
+						['<C-Space>'] = cmp.mapping.complete(),
+						['<C-e>'] = cmp.mapping.abort(),
+						['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					}),
+					completion = {
+						completeopt = "menu,noselect"
+					},
+					formatting = {
+						fields = { "kind", "abbr", "menu" },
+						format = function(entry, vim_item)
+							local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+							local strings = vim.split(kind.kind, "%s", { trimempty = true })
+							kind.kind = " " .. strings[1] .. " "
+							kind.menu = "    (" .. strings[2] .. ")"
+
+							return kind
+						end,
+					},
+					sources = cmp.config.sources({
+						{ name = 'nvim_lsp' },
+						{ name = 'vsnip' }, -- For vsnip users.
+					}, {
+						{ name = 'buffer' },
+					})
+				})
+			end
+		}
 		use 'mtdl9/vim-log-highlighting'
     use { 
 	    'akinsho/flutter-tools.nvim',
 	    branch = 'main',
 	    opt = true,
+			requires = 'nvim-lua/plenary.nvim',
 	    config = function ()
+				local lsp = require('helpers_lsp')
+				print("LSP LOADED")
 				require("flutter-tools").setup{
 					flutter_path='/usr/bin/flutter',
 					widget_guides = {
@@ -84,6 +170,14 @@ require('packer').startup(function()
 						}
 					},
 					lsp = {
+						color = { -- show the derived colours for dart variables
+							enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+							background = false, -- highlight the background
+							foreground = false, -- highlight the foreground
+							virtual_text = true, -- show the highlight using virtual text
+							virtual_text_str = "■", -- the virtual text character to highlight
+						},
+						on_attach = lsp.on_attach,
 						settings = {
 							dart = {
 								enableSdkFormatter=true,
@@ -95,7 +189,35 @@ require('packer').startup(function()
 	    end,
 	    ft = 'dart'
     }
-    use 'kyazdani42/nvim-tree.lua'
+    use {
+			'kyazdani42/nvim-tree.lua',
+			config = function ()
+				require 'nvim-tree'.setup {
+					actions = {
+						open_file = {
+							quit_on_open = true,
+							resize_window = true,
+						},
+					},
+					update_focused_file = {
+						enable = true,
+					},
+					git = {
+						ignore = false,
+					},
+					diagnostics = {
+						enable = true,
+						show_on_dirs = true,
+						icons = {
+							hint = "",
+							info = "",
+							warning = "",
+							error = "",
+						}
+					},
+				}
+			end
+		}
     use {
 	    'janko/vim-test',
 	    opt = true,
@@ -104,7 +226,6 @@ require('packer').startup(function()
     use 'folke/trouble.nvim'
     use {
 			'mfussenegger/nvim-dap',
-			opt = true,
 			config = function()
 				local dap = require('dap')
 
@@ -125,8 +246,7 @@ require('packer').startup(function()
 							cwd = "${workspaceFolder}",
 					}
 				}
-			end,
-			ft={'dart'}
+			end
 		}
     use {
 	    'digitaltoad/vim-pug',
@@ -134,99 +254,11 @@ require('packer').startup(function()
 	    ft='pug'
     }
 end)
-local cmp = require'cmp'
-local lspkind = require('lspkind')
 
 vim.api.nvim_command("autocmd BufWritePre *.dart lua vim.lsp.buf.formatting_sync(nil, 1000)")
 vim.api.nvim_command("autocmd Filetype typescript setlocal ts=2 sw=2 noexpandtab")
 vim.api.nvim_command("autocmd Filetype typescriptreact setlocal ts=2 sw=2 noexpandtab")
 vim.api.nvim_command("syntax on")
 vim.api.nvim_command("filetype plugin indent on")
-vim.api.nvim_command("colorscheme sonokai")
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-		end,
-	},
-	window = {
-		completion = {
-			winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-      col_offset = -3,
-      side_padding = 0,
-		},
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
-	completion = {
-    completeopt = "menu,noselect"
-  },
-	formatting = {
-		fields = { "kind", "abbr", "menu" },
-    format = function(entry, vim_item)
-      local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
-      local strings = vim.split(kind.kind, "%s", { trimempty = true })
-      kind.kind = " " .. strings[1] .. " "
-      kind.menu = "    (" .. strings[2] .. ")"
-
-      return kind
-    end,
-  },
-	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		{ name = 'vsnip' }, -- For vsnip users.
-	}, {
-		{ name = 'buffer' },
-	})
-})
 
 require("trouble").setup {}
-require('telescope').setup{
-	pickers = {
-		find_files = {
-			previewer = false,
-		}
-	},
-   defaults = {
-			 layout_config = {
-				 prompt_position = 'top',
-			 },
-       sorting_strategy = 'ascending',
-   },
-}
-
-
-require 'nvim-tree'.setup {
-	actions = {
-		open_file = {
-			quit_on_open = true,
-			resize_window = true,
-		},
-	},
-	update_focused_file = {
-		enable      = true,
-	},
-	git = {
-		ignore = false,
-	},
-	diagnostics = {
-    enable = true,
-		show_on_dirs = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    }
-  },
-}
-
-require('gitsigns').setup {
-	current_line_blame = true,
-	numhl = true,
-}
